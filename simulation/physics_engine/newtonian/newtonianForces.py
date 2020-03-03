d34709ec18ea5314a0ac954b03cc6ab9b094c9d2
Christopher Rowe <chris.rowe19@outlook.com>
2020-03-03 17:29:31 +0000

    Removes inapropriate use of bob a a variable name.

--- simulation/physics_engine/newtonian/newtonianForces.py
+++ simulation/physics_engine/newtonian/newtonianForces.py
@@ -1,4 +1,4 @@
-import simulation.physics_engine.constants as bob
+from simulation.physics_engine.constants import G
 
 def forceFromMassAcceleration(mass, acceleration):
     """Finds force using f = ma """
@@ -14,13 +14,13 @@ def accelerationFromForceMass(force, mass):
 
 def forceBetweenTwoLargeBodies(mass1,mass2,radius):
     """Finds force using F1 = F2 = G ((m1 x m2)/r^2)"""
-    return bob.G * ((mass1 * mass2)/radius**2)
+    return G * ((mass1 * mass2)/radius**2)
 
 def radiusFromForceMassBetweenTwoLargeBodies(mass1,mass2,force):
     """Finds radius from F1 = F2 = G ((m1 x m2)/r^2)"""
-    return (bob.G * ((mass1 * mass2)/force))**1/2
+    return (G * ((mass1 * mass2)/force))**1/2
 
 def massFromRadiusForceBetweenTwoLargeBodies(mass,force,radius):
     """Finds the other mass from F1 = F2 = G ((m1 x m2)/r^2)"""
     #return (bob.G * ((mass1 * mass2)/force))**1/2
-    return 1/(bob.G * (mass/(force * radius**2)))
+    return 1/(G * (mass/(force * radius**2)))
