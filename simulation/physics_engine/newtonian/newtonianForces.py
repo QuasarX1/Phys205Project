2db071369827b8d953e282ee24295655b45072ef
Christopher Rowe <chris.rowe19@outlook.com>
2020-03-06 12:00:16 +0000

    Alters paramiter spacings.

--- simulation/physics_engine/newtonian/newtonianForces.py
+++ simulation/physics_engine/newtonian/newtonianForces.py
@@ -12,15 +12,15 @@ def accelerationFromForceMass(force, mass):
     """Finds acceleration using a = f/m"""
     return force / mass
 
-def forceBetweenTwoLargeBodies(mass1,mass2,radius):
+def forceBetweenTwoLargeBodies(mass1, mass2, radius):
     """Finds force using F1 = F2 = G ((m1 x m2)/r^2)"""
     return G * ((mass1 * mass2) / radius**2)
 
-def radiusFromForceMassBetweenTwoLargeBodies(mass1,mass2,force):
+def radiusFromForceMassBetweenTwoLargeBodies(mass1, mass2, force):
     """Finds radius from F1 = F2 = G ((m1 x m2)/r^2)"""
     return (G * ((mass1 * mass2) / force))**1 / 2
 
-def massFromRadiusForceBetweenTwoLargeBodies(mass,force,radius):
+def massFromRadiusForceBetweenTwoLargeBodies(mass, force, radius):
     """Finds the other mass from F1 = F2 = G ((m1 x m2)/r^2)"""
     #return (bob.G * ((mass1 * mass2)/force))**1/2
     return 1/(G * (mass / (force * radius**2)))
