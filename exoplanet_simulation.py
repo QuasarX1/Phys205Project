9acf7075655e5007c4f86f9a27c8cde7f353d3e7
Christopher Rowe <chris.rowe19@outlook.com>
2020-03-12 13:42:40 +0000

    Experements with the orbital simulation with two stars.

--- exoplanet_simulation.py
+++ exoplanet_simulation.py
@@ -4,11 +4,11 @@ from star import Star
 
 simulation = sim.Simulation()
 
-simulation.getCamera().setLocation(pygame.Vector3(0, 200, 0))
-simulation.getCamera().setFacing(pygame.Vector3(0, -1, 0))
-simulation.getCamera().setVertical(pygame.Vector3(0, 0, 1))
+#simulation.getCamera().setLocation(pygame.Vector3(0, 200, 0))
+#simulation.getCamera().setFacing(pygame.Vector3(0, -1, 0))
+#simulation.getCamera().setVertical(pygame.Vector3(0, 0, 1))
 
-#simulation.getCamera().setLocation(pygame.Vector3(0, 0, -200))
+simulation.getCamera().setLocation(pygame.Vector3(0, 0, -200))
 
 HUD = sim.layer.Layer_2D(pygame.Surface((500, 500), pygame.SRCALPHA))
 simulation.addLayer("HUD", HUD)
@@ -26,7 +26,7 @@ simulation_layer.addEntity("target_star", target_star)
 test_star = Star(radius = 10,
                  temperature = 100,
                  mass = 10000,#1.9891 * 10**30,#500000,
-                   initial_velocity = pygame.Vector3(0, 0, -20),
+                 initial_velocity = pygame.Vector3(0, 0, 80),
                  location = pygame.Vector3(400, 0, 0))
 simulation_layer.addEntity("test_star", test_star)
 
