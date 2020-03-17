3fb207522fa3077b59acaf9d0f5a3b0537ffd92d
Kyeman Fong <tamriel00@outlook.com>
2020-03-17 14:33:46 +0000

    Added Comments to explain what each equation does in the optics section

--- simulation/physics_engine/optics/properties.py
+++ simulation/physics_engine/optics/properties.py
@@ -3,14 +3,30 @@ import numpy as np
 def flux_sphere(luminosity, distance):
     return luminosity / (4 * np.pi * (distance**2))
 
+'''
+Calculates flux from the luminosity of the star and the distance the star is from the planet
+'''
+
 def flux_inverse_square(flux_B, distance_a, distance_b):
     return flux_b * ((distance_b / distance_a)**2)
+
+'''
+Converts the flux from one distance, to a flux of the same star, at a different distance
+'''
 
 def luminosity_boltzmann(surface_area, surface_temperature):
     return (5.6704 * 10**-8) * surface_area * surface_temperature
 
+'''
+Calculates total Luminosity from the surface temperature of the star and its surface area
+'''
+
 def luminosity_flux_density(flux_density, illuminated_area):
     return flux_density * illuminated_area
+
+'''
+Calculates Luminosity from flux density and the luminated area 
+'''
 
 def luminosity_sphere(flux, star_radius):
     return flux * 4 * np.pi * (star_radius**2)
@@ -24,6 +40,7 @@ def wavelength_doppler(redshift_factor, apparent_wavelength):
 def peak_wavelength_surface_temp(surface_temperature):
     return ((3 * 10**8) / (surface*temperature * 5.879 * 10**10)
 
-
+def radiance_wavelength_temperature(peak_wavelength, surface_temperature):
+    return ((2 * 6.63 * (10**-34) / (peak_wavelength)) * (1/(np.exp(6.63 * 10**-34 / (1.38 * 10**-23) * surface_temperature))))
 
 #TODO: add function to calculate luminosity in a given wavelength given a temp of a black body
