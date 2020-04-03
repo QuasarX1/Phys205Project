73975d733dbbe231b140c928e3e2800b8d909ebd
Kyeman Fong <tamriel00@outlook.com>
2020-04-03 13:13:33 +0100

    amended a few optical equations, removed an unneccesary function

--- simulation/physics_engine/optics/blackbody.py
+++ simulation/physics_engine/optics/blackbody.py
@@ -7,34 +7,20 @@ def peak_wavelength(surface_temperature):
     '''
     return speed_of_light / (surface_temperature * 5.879 * 10**10)#TODO: is this Wiens constant?? if so needs to be put in the constants file
 
-def luminosity(wavelength, surface_temperature):
+def radiance(wavelength, surface_temperature):
     """
-    Calculates the luminosity of radiation emitted by a blackbody of a given temperature in a given wavelength.
+    Calculates the luminosity of a specific wavelength emitted from a star, not the total luminosity
 
     Paramiters:
         float wavelength -> The wavelength in meters
         float surface_temperature -> The blackbody's temperature in kelvin
     """
-    return 2 * planksConstant * speed_of_light / wavelength**3 / (np.exp(planksConstant / wavelength * surface_temperature * boltzmann_constant) - 1)
+    return 2 * planksConstant * speed_of_light / wavelength**5 / (np.exp(planksConstant / wavelength * surface_temperature * boltzmann_constant) - 1)
 
-
-
-
-
-#TODO: what do the functions below do??? Could do with a more in depth description plus physics equasion in the comment for each
-
-def peak_luminosity(surface_temperature):
+def total_luminosity(peak_wavelength, star_radius):
     '''
-    Calculates the radience (or effective intensity) of the peak wavelength of a star, from the peak wavelength and the surface temperature, likely shouldn't be needed unless light emission is calculated as an array
-    '''
-    peak_wavelength = peak_wavelength_surface_temp(surface_temperature)
-
-    #TODO: should this be the same as the "luminosity" function above?
-    # if so one or the other needs to be corrected and this line replaced with a function call instead
-    return (2 * planksConstant / peak_wavelength) * np.exp(planksConstant / boltzmann_constant * surface_temperature)
-
-def luminosity_peak_wavelength(peak_wavelength, star_radius):
-    '''
-    Calculates luminosity of the star from the star's radius and the peak wavelength of emitted light
+    Calculates the total luminosity of a star from the star's radius and the peak wavelength
     '''
     return (4 * np.pi * (star_radius**2) * (5.67 * 10**-8) * ((0.0029 / peak_wavelength)**4))
+
+
