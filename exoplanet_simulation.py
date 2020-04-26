d3868a9ebabcece2e7a4b0bbdd1e4fa34c5d9e0b
Kyeman Fong <tamriel00@outlook.com>
2020-04-26 17:08:12 +0100

    Added and completed the Solar_System.xml file for the simulation

--- exoplanet_simulation.py
+++ exoplanet_simulation.py
@@ -11,7 +11,7 @@ import uuid
 import simulation as sim
 from simulation import Simulation
 from simulation import RenderMode
-from simulation.entities.astro_bodies import Star, Planet, Sun, Mercury, Venus, Earth
+from simulation.entities.astro_bodies import Star, Planet, Sun, Mercury, Venus, Earth, Mars
 from simulation.entities.prefabs import UnitCube_Wireframe
 from simulation.logging import GraphingLogger, PositionLogger, VelocityLogger, SeperationLogger
 
